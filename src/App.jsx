eefe8008ec03ba478ef621ffe240589e3e66f766
AliyanAli911 <aliyanali723033@gmail.com>
2025-04-05 02:13:56 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './component/Header'
+// import Sidebar from './component/Sidebar'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='bg-[#D3D3D3] min-h-screen'>
+      <Header/>
+     
+    </div>
+      
+  )
+}
+export default App
